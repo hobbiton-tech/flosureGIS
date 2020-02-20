def35e55b50a826e8e7b906a410dbb424cbc4753
Daniel SItali <danielmsitali@outlook.com>
2020-02-20 17:25:30 +0200

    add modules Co-authored-by: rayne22 <changalesa8@gmail.com>

---
+++ src/app/claims/claims.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClaimsComponent } from './claims.component';
+
+
+
+@NgModule({
+  declarations: [ClaimsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ClaimsModule { }
---
+++ src/app/underwriting/underwriting.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UnderwritingComponent } from './underwriting.component';
+
+
+
+@NgModule({
+  declarations: [UnderwritingComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class UnderwritingModule { }
