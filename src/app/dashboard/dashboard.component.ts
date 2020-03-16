6eb831707235f1ab16a80941384289ea870ebc33
Daniel SItali <danielmsitali@outlook.com>
2020-03-16 17:26:25 +0200

    add loading

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,43 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { DashboardService } from './services/dashboard.service'
+import { DashboardService } from './services/dashboard.service';
 import { ClientsService } from '../clients/services/clients.service';
 import { ClaimsService } from '../claims/services/claims-service.service';
 import { PoliciesService } from '../underwriting/services/policies.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+    clientsCount: number = 0;
+    claimsCount: number = 0;
+    policiesCount: number = 0;
+    cancelledPoliciesCount: number;
+    commissionEarned: number = 0;
 
-  clientsCount: number;
-  claimsCount: number;
-  policiesCount: number;
-  cancelledPoliciesCount : number;
-  commissionEarned: number = 0;
+    clientsLoading: boolean = true;
+    claimsLoading: boolean = true;
+    policiesLoading: boolean = true;
 
-  constructor(
-    private readonly dashboardService: DashboardService,
-    private clientsService: ClientsService,
-    private claimsService: ClaimsService,
-    private policiesService: PoliciesService) { }
+    constructor(
+        private readonly dashboardService: DashboardService,
+        private clientsService: ClientsService,
+        private claimsService: ClaimsService,
+        private policiesService: PoliciesService
+    ) {}
 
-  ngOnInit(): void {
-    this.clientsService.getClients().subscribe((clients) => {
-      this.clientsCount = clients.length;
-    })
+    ngOnInit(): void {
+        this.clientsService.getClients().subscribe(clients => {
+            this.clientsCount = clients.length;
+            this.clientsLoading = false;
+        });
 
-    this.claimsService.getClaims().subscribe((claims) => {
-      this.claimsCount = claims.length;
-    })
+        this.claimsService.getClaims().subscribe(claims => {
+            this.claimsCount = claims.length;
+            this.claimsLoading = false;
+        });
 
-    this.policiesService.getPolicies().subscribe((policies) => {
-      this.policiesCount = policies.length
-    })
-  }
-  
+        this.policiesService.getPolicies().subscribe(policies => {
+            this.policiesCount = policies.length;
+            this.policiesLoading;
+        });
+    }
 
-  // clientsCount = this.dashboardService.getClientsCount();
-
+    // clientsCount = this.dashboardService.getClientsCount();
 }
