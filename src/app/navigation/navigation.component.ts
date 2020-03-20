aa75853f6a9495a14ba33890dd5be34c555fe004
Daniel SItali <danielmsitali@outlook.com>
2020-03-20 10:30:53 +0200

    added logout to header

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-navigation',
@@ -8,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class NavigationComponent implements OnInit {
     isCollapsed = false;
 
-    constructor() {}
+    constructor(private router: Router) {}
 
     ngOnInit(): void {}
+
+    logout(): void {
+        this.router.navigateByUrl('/');
+    }
 }
