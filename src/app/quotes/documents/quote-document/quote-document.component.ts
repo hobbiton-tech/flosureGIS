289c468a2806bcbfd54ae4d1496d7aa55580ae85
Daniel SItali <danielmsitali@outlook.com>
2020-04-15 16:18:30 +0200

    updated quote document

---
+++ src/app/quotes/documents/quote-document/quote-document.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-document',
+  templateUrl: './quote-document.component.html',
+  styleUrls: ['./quote-document.component.scss']
+})
+export class QuoteDocumentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
