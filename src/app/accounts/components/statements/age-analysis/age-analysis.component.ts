127e7af4f32505a28855bc9b96493f9b91fb0cf3
Daniel Sitali <danielmsitali@outlook.com>
2020-09-15 08:36:23 +0200

    ddd

--- src/app/accounts/components/statements/age-analysis/age-analysis.component.ts
+++ src/app/accounts/components/statements/age-analysis/age-analysis.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import html2canvas from "html2canvas";
-import jspdf from "jspdf";
+import html2canvas from 'html2canvas';
+import jspdf from 'jspdf';
 import { ICorporateClient, IIndividualClient } from '../../../../clients/models/clients.model';
 import { ClientsService } from '../../../../clients/services/clients.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -52,29 +52,110 @@ export class AgeAnalysisComponent implements OnInit {
 
     const secondDate = new Date(value.transaction_date);
     const time = 30 * 60 * 60 * 24 * 1000;
-    if ((todayDate.getTime() - secondDate.getTime()) < time) {
-      console.log({dateCheck: `Not greater than ${30} days`});
-    } else {
-      console.log({dateCheck: `Greater than ${30} days`});
+    console.log('Time>>>', (todayDate.getTime() - secondDate.getTime()), time);
+    if ((todayDate.getTime() - secondDate.getTime()) <= time  && value.balance > 0) {
+      return value.balance;
+    } else  {
+      return 0;
     }
   }
   ageSixty(value) {
+    const todayDate = new Date();
+    // const diff = Math.abs(value.transaction_date.getTime() - todayDate.getTime());
+    // const diffDays = Math.ceil(diff / ((1000 * 3600 * 24)) * 30);
+
+    const secondDate = new Date(value.transaction_date);
+    const time = 60 * 60 * 60 * 24 * 1000;
+    const time2 = 30 * 60 * 60 * 24 * 1000;
+    if ((todayDate.getTime() - secondDate.getTime()) < time  && (todayDate.getTime() - secondDate.getTime()) > time2   && value.balance > 0) {
+      return value.balance;
+    } else  {
+      return 0;
+    }
 
   }
   ageNinety(value) {
+    const todayDate = new Date();
+    // const diff = Math.abs(value.transaction_date.getTime() - todayDate.getTime());
+    // const diffDays = Math.ceil(diff / ((1000 * 3600 * 24)) * 30);
+
+    const secondDate = new Date(value.transaction_date);
+    const time = 90 * 60 * 60 * 24 * 1000;
+    const time2 = 60 * 60 * 60 * 24 * 1000;
+    if ((todayDate.getTime() - secondDate.getTime()) < time  && (todayDate.getTime() - secondDate.getTime()) > time2  && value.balance > 0) {
+      return value.balance;
+    } else  {
+      return 0;
+    }
 
   }
   ageOneTwenty(value) {
+    const todayDate = new Date();
+    // const diff = Math.abs(value.transaction_date.getTime() - todayDate.getTime());
+    // const diffDays = Math.ceil(diff / ((1000 * 3600 * 24)) * 30);
+
+    const secondDate = new Date(value.transaction_date);
+    const time = 120 * 60 * 60 * 24 * 1000;
+    const time2 = 90 * 60 * 60 * 24 * 1000;
+    if ((todayDate.getTime() - secondDate.getTime()) < time  && (todayDate.getTime() - secondDate.getTime()) > time2  && value.balance > 0) {
+      return value.balance;
+    } else  {
+      return 0;
+    }
 
   }
   ageOneEighty(value) {
+    const todayDate = new Date();
+    // const diff = Math.abs(value.transaction_date.getTime() - todayDate.getTime());
+    // const diffDays = Math.ceil(diff / ((1000 * 3600 * 24)) * 30);
+
+    const secondDate = new Date(value.transaction_date);
+    const time = 180 * 60 * 60 * 24 * 1000;
+    const time2 = 120 * 60 * 60 * 24 * 1000;
+    if ((todayDate.getTime() - secondDate.getTime()) < time  && (todayDate.getTime() - secondDate.getTime()) > time2  && value.balance > 0) {
+      return value.balance;
+    } else  {
+      return 0;
+    }
 
   }
   ageAnnual(value) {
+    const todayDate = new Date();
+    // const diff = Math.abs(value.transaction_date.getTime() - todayDate.getTime());
+    // const diffDays = Math.ceil(diff / ((1000 * 3600 * 24)) * 30);
+
+    const secondDate = new Date(value.transaction_date);
+    const time = 365 * 60 * 60 * 24 * 1000;
+    const time2 = 180 * 60 * 60 * 24 * 1000;
+    if ((todayDate.getTime() - secondDate.getTime()) < time  && (todayDate.getTime() - secondDate.getTime()) > time2  && value.balance > 0) {
+      return value.balance;
+    } else  {
+      return 0;
+    }
 
   }
   ageOverAnnual(value) {
+    const todayDate = new Date();
+    // const diff = Math.abs(value.transaction_date.getTime() - todayDate.getTime());
+    // const diffDays = Math.ceil(diff / ((1000 * 3600 * 24)) * 30);
 
+    const secondDate = new Date(value.transaction_date);
+    const time = 365 * 60 * 60 * 24 * 1000;
+    if ((todayDate.getTime() - secondDate.getTime()) > time  && value.balance > 0) {
+      return value.balance;
+    } else  {
+      return 0;
+    }
+
+  }
+
+  getTotalOutstanding(value) {
+    const total = Number(this.ageThirty(value)) + Number(this.ageSixty(value)) + Number(this.ageNinety(value)) +
+      Number(this.ageOneTwenty(value)) + Number(this.ageOneEighty(value)) + Number(this.ageAnnual(value)) +
+      Number(this.ageOverAnnual(value));
+    console.log('Total', total);
+
+    return total;
   }
 
 
