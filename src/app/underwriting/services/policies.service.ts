18a034bdefb88208f5f7a1eace561f96a9057b5e
Changa Lesa <changalesa8@gmail.com>
2020-11-13 08:53:23 +0200

    url

--- src/app/underwriting/services/policies.service.ts
+++ src/app/underwriting/services/policies.service.ts
@@ -22,9 +22,9 @@ import { AccountService } from 'src/app/accounts/services/account.service';
 import { IClass } from 'src/app/settings/components/product-setups/models/product-setups-models.model';
 import { InsuranceClassHandlerService } from './insurance-class-handler.service';
 
-const BASE_URL = 'https://savenda.flosure-api.com/';
+const BASE_URL = 'https://savenda.flosure-api.com';
 
-// const BASE_URL = 'https://savenda.flosure-api.com/';
+// const BASE_URL = 'https://savenda.flosure-api.com';
 
 interface IDebitNoteResult {
     invoiceNumber: string;
@@ -86,14 +86,14 @@ export class PoliciesService implements OnDestroy {
     }
 
     // getPolicies(): Observable<Policy[]> {
-    //     return this.http.get<Policy[]>('https://savenda.flosure-api.com//policy');
+    //     return this.http.get<Policy[]>('https://savenda.flosure-api.com/policy');
 
     // }
 
     // getPolicyById(policyId: string): Observable<Policy> {
     //     return this.http.get<Policy>(
 
-    //         `https://savenda.flosure-api.com//policy/${policyId}`
+    //         `https://savenda.flosure-api.com/policy/${policyId}`
 
     //     );
     //     return this.policiesCollection.doc<Policy>(policyId).valueChanges();
@@ -101,7 +101,7 @@ export class PoliciesService implements OnDestroy {
 
     updatePolicy(policy: Policy): Observable<Policy> {
         return this.http.put<Policy>(
-            `https://savenda.flosure-api.com//policy/${policy.id}`,
+            `https://savenda.flosure-api.com/policy/${policy.id}`,
             policy
         );
     }
@@ -109,7 +109,7 @@ export class PoliciesService implements OnDestroy {
     // backup policies
     createBackupPolicy(policy: Policy): Observable<Policy> {
         return this.http.post<Policy>(
-            'https://savenda.flosure-api.com//policy',
+            'https://savenda.flosure-api.com/policy',
             policy
         );
     }
